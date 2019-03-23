45f4336553566353859cf4e4e46d77127f8b9d92
Josh Wulf <josh@magikcraft.io>
2019-03-23 13:33:14 +1000

    WIP commit

--- bin/run.sh
+++ bin/run.sh
@@ -5,12 +5,17 @@ if [ ! -d "$HOME/.mct1-worlds" ]; then
     git clone --depth 1 https://github.com/Magikcraft/mct1-worlds.git "$HOME/.mct1-worlds"
 fi
 
-SCRIPTDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+    DIR="$(cd -P "$(dirname "$SOURCE")" && pwd)"
+    SOURCE="$(readlink "$SOURCE")"
+    [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
 
 if [ -x "$(command -v docker)" ]; then
     docker run -it -p 25565:25565 \
         --mount source=scriptcraft-cache,target=/server/cache \
-        --mount type=bind,src="${SCRIPTDIR}/../node_modules",dst=/server/scriptcraft-plugins \
+        --mount type=bind,src="${DIR}/../node_modules",dst=/server/scriptcraft-plugins \
         --mount type=bind,src=$HOME/.mct1-worlds,dst=/server/worlds \
         magikcraft/scriptcraft
 else
